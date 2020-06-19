a236d8a91abec00f9bf571a135aae86520e3a90e
Diego Ruiz <diego.ruiz@C02Z20WGLVDQ.local>
2020-06-19 11:27:07 -0400

    script to help track performance

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+secs_to_human() {
+    if [[ -z ${1} || ${1} -lt 60 ]] ;then
+        min=0 ; secs="${1}"
+    else
+        time_mins=$(echo "scale=2; ${1}/60" | bc)
+        min=$(echo ${time_mins} | cut -d'.' -f1)
+        secs="0.$(echo ${time_mins} | cut -d'.' -f2)"
+        secs=$(echo ${secs}*60|bc|awk '{print int($1+0.5)}')
+    fi
+    echo "Time Elapsed : ${min} minutes and ${secs} seconds."
+}
+
+start=$(date +%s)
+docker build -t test-ui-build .
+secs_to_human "$(($(date +%s) - ${start}))"
